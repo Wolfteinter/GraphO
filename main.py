8c4cb4ac0853761b0ae12060dc9a285528e36644
Wolfteinter <onderfra@gmail.com>
2020-10-20 00:27:50 -0500

    to_segments function is complete

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import graphO as o
-print(   o.to_disjointness_graph([(1,2),(4,5)])   )
+print(o.to_segments([(1,2),(4,5),(7,4),(1,5),(4,7)]))
+#print(o.to_disjointness_graph([(1,2),(4,5),(7,4),(1,5),(4,7),]))
