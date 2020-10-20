bc789f5742731d09a1d1bcac2925feb7181ce38b
Wolfteinter <onderfra@gmail.com>
2020-10-20 02:19:32 -0500

    to_disjointness_graph function is complete

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import graphO as o
-print(o.to_segments([(1,2),(4,5),(7,4),(1,5),(4,7)]))
-#print(o.to_disjointness_graph([(1,2),(4,5),(7,4),(1,5),(4,7),]))
+#points = [(0,0),(5,1),(5,3),(2,4),(2,2)]
+points = [(2,4),(5,3),(5,1),(0,0),(2,2)]
+segments = o.to_segments(points)
+print(list(enumerate(segments)))
+print(o.to_disjointness_graph(points))
